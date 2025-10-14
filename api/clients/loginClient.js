c595be5af808425f199528d3721369fbe1cce3c5
Timofey Litvinov <otdelprodajclock@yandex.ru>
2025-10-15 01:23:33 +0300

    Allow login client to forward axios options

--- api/clients/loginClient.js
+++ api/clients/loginClient.js
@@ -12,47 +12,59 @@ export class LoginClient extends PublicClient {
      * @param {string} password
      * @returns {Promise<AxiosResponse>}
      */
-    async signInRequest(login, password) {
+    async signInRequest(login, password, options = {}) {
         const payload = { login, password };
 
-        const response = await this.post('/auth/client/sign-in/request', payload);
+        const response = await this.post('/auth/client/sign-in/request', payload, options);
 
         const setCookie = response.headers['set-cookie'];
         if (setCookie) {
-            this.cookies = setCookie.map(c => c.split(';')[0]).join('; ');
+            this.cookies = this.#mergeCookies(setCookie);
         }
 
         return response;
     }
 
-    async signInConfirm(login, confirmationCode) {
+    async signInConfirm(login, confirmationCode, options = {}) {
         const response = await this.post(
             '/auth/client/sign-in/confirm',
             { confirmationCode, login },
-            {
-                headers: {
-                    Cookie: this.cookies,
-                },
-            },
+            this.#withSessionHeaders(options),
         );
 
         const setCookie = response.headers['set-cookie'];
         if (setCookie) {
-            this.cookies += '; ' + setCookie.map(c => c.split(';')[0]).join('; ');
+            this.cookies = this.#mergeCookies(setCookie, this.cookies);
         }
 
         return response;
     }
 
-    async signInFinalize(pin = '0000') {
+    async signInFinalize(pin = '0000', options = {}) {
         return await this.post(
             '/auth/client/sign-in/finalize',
             { PIN: pin },
-            {
-                headers: {
-                    Cookie: this.cookies,
-                },
-            },
+            this.#withSessionHeaders(options),
         );
+    }
+
+    #withSessionHeaders(options = {}) {
+        const headers = { ...(options.headers ?? {}) };
+
+        if (this.cookies && headers.Cookie === undefined) {
+            headers.Cookie = this.cookies;
+        }
+
+        return { ...options, headers };
+    }
+
+    #mergeCookies(newCookies, existing = '') {
+        const serialized = newCookies.map(c => c.split(';')[0]).join('; ');
+
+        if (!existing) {
+            return serialized;
+        }
+
+        return [existing, serialized].filter(Boolean).join('; ');
     }
 }

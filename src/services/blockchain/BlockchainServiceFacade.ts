3d851f7d43c819ff5b7e2ceb22a081ea6acf72eb
Timofey Litvinov <otdelprodajclock@yandex.ru>
2025-10-16 22:43:00 +0300

    Add network-specific service factory for blockchain facade

--- src/services/blockchain/BlockchainServiceFacade.ts
+++ src/services/blockchain/BlockchainServiceFacade.ts
@@ -4,6 +4,10 @@ import {
     TonTransactionService,
     TronTransactionService
 } from "./index";
+import {Currencies, CurrencyKey, CurrencyType} from "../../model/Currency";
+import {Network} from "../../model/Network";
+
+type Currency = typeof Currencies[CurrencyKey];
 
 export class BlockchainServiceFacade {
     readonly btc: BtcTransactionService;
@@ -16,5 +20,28 @@ export class BlockchainServiceFacade {
         this.eth = new EthTransactionService();
         this.tron = new TronTransactionService();
         this.ton = new TonTransactionService();
+    }
+
+    getInstance(currency: Currency):
+        | BtcTransactionService
+        | EthTransactionService
+        | TronTransactionService
+        | TonTransactionService {
+        if (currency.type === CurrencyType.FIAT) {
+            throw new Error("Fiat currencies are not supported");
+        }
+
+        switch (currency.network) {
+            case Network.BTC:
+                return this.btc;
+            case Network.ETH:
+                return this.eth;
+            case Network.TRON:
+                return this.tron;
+            case Network.TON:
+                return this.ton;
+            default:
+                throw new Error(`Unsupported network: ${currency.network}`);
+        }
     }
 }
